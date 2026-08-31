372f7dad09dc02b8acbafd0f39e76398d6738a2f
grantsnider <gsnider@hcc-nd.edu>
2026-08-31 18:49:02 -0400

    SortingAlgorithm & BubbleSort are now generic.

--- src/assignments/sorting/SortingAlgorithm.java
+++ src/assignments/sorting/SortingAlgorithm.java
@@ -2,30 +2,33 @@ package assignments.sorting;
 
 import java.util.Arrays;
 
-/// Superclass to ensure all sorting algorithms operate in a consistent manner.
-/// 
-/// We use "interfaces" for data structures, but we use this "abstract class" for sorting algorithms.
-/// The chief differences are that a class can "implement" any number of interfaces but only "extend" one class,
-///  and that abstract classes are allowed to implement default behavior.
-/// We take advantage of this fact to implement two versions of every sort:
-///  an "in-place" version called `sort` which modifies the input array, and
-///  an "out-of-place" version called `sorted` which constructs a new array.
-/// The subclasses you create will only need to override the "in-place" version.
-/// 
-/// To create a class implementing your own sorting algorithm,
-/// 1. Create a new class file in your "`sorting`" package. E.g. `BubbleSort.java`.
-/// 2. Extend this class in your class definition. E.g. "`class BubbleSort extends SortingAlgorithm`"
-/// 3. Implement the "`sort`" method.
-/// 4. Write a main function in your class file which, at minimum, calls the "`validate`" method. E.g. `SortingAlgorithm.validate(new BubbleSort())`)
-public abstract class SortingAlgorithm {
+/**  Superclass to ensure all sorting algorithms operate in a consistent manner.
+ 
+ We use "interfaces" for data structures, but we use this "abstract class" for sorting algorithms.
+ The chief differences are that a class can "implement" any number of interfaces but only "extend" one class,
+  and that abstract classes are allowed to implement default behavior.
+ We take advantage of this fact to implement two versions of every sort:
+  an "in-place" version called `sort` which modifies the input array, and
+  an "out-of-place" version called `sorted` which constructs a new array.
+ The subclasses you create will only need to override the "in-place" version.
+ 
+ To create a class implementing your own sorting algorithm,
+ 1. Create a new class file in your "`sorting`" package. E.g. `BubbleSort.java`.
+ 2. Extend this class in your class definition. E.g. "`class BubbleSort extends SortingAlgorithm`"
+ 3. Implement the "`sort`" method.
+ 4. Write a main function in your class file which, at minimum, calls the "`validate`" method.
+*/
+public abstract class SortingAlgorithm<T> {
     /**
+     * Changed the Sorting Algorithm to now be generalized.
+     * 
      * Sort an array in-place.
      * 
      * Post-condition: `array` is sorted in ascending order.
      * 
      * @param array the array to sort
      */
-    public abstract void sort(Integer[] array);
+    public abstract void sort(T[] array);
 
     /**
      * Construct a sorted version of an array.
@@ -33,8 +36,8 @@ public abstract class SortingAlgorithm {
      * @param array the array to sort
      * @return the sorted array
      */
-    public Integer[] sorted(Integer[] array) {
-        Integer[] copiedArray = Arrays.copyOf(array, array.length);
+    public T[] sorted(T[] array) {
+        T[] copiedArray = Arrays.copyOf(array, array.length);
         sort(copiedArray);
         return copiedArray;
     }
@@ -56,7 +59,7 @@ public abstract class SortingAlgorithm {
      *  including potentially tricky features like negative numbers and duplicates.
      * @param algorithm a freshly-initialized SortingAlgorithm object
      */
-    public static void validate(SortingAlgorithm algorithm) {
+    public static void validate(SortingAlgorithm<Integer> algorithm) {
         // Construct a sample array of numbers, including negatives and duplicates.
         Integer[] random_array = {-7, 4, 3, 5, -9, 1, -6, 7, -5, -9, -1, 1, 7, 7, 6};
         Integer[] sorted_array = {-9, -9, -7, -6, -5, -1, 1, 1, 3, 4, 5, 6, 7, 7, 7};
