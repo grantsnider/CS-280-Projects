29419f85c36c50aeb98438b117a9c4b2c8997f6a
grantsnider <gsnider@hcc-nd.edu>
2026-09-15 18:54:26 -0400

    Updated LinkedList and Test.java, added comments, and fixed the ERAFibonacci output.

--- src/assignments/algorithms/ERAFibonacci.java
+++ src/assignments/algorithms/ERAFibonacci.java
@@ -3,12 +3,12 @@ package assignments.algorithms;
 public class ERAFibonacci {
 
     /**
-    * Computes the nth Fibonacci number with dyanmic programming.
+    * Computes the nth Fibonacci number using dyanmic programming.
     * 
     * @param n the Fibonacci index
     * @return the nth Fibonacci number
     */
-    public static int Fibonacci(int n) {
+    public static int fibonacci(int n) {
         if (n <= 1)
             return 1;
         int last = 1;
@@ -25,21 +25,27 @@ public class ERAFibonacci {
     }
 
     /**
-    * The Recursive Fibonacci
+    * Computes the nth Fibonacci number using recursion.
+    * 
+    * @param x the Fibonacci index
+    * @return the nth Fibonacci number
     */
-    public static long FibRec(int x) {
+    public static long fibRec(int x) {
         if (x <= 1)
             return 1;
         else
-           return FibRec(x - 1) + FibRec(x - 2);
+           return fibRec(x - 1) + fibRec(x - 2);
     }
 
     /**
-     * Binet Formula
+     * Computes Binets formula
+     * 
+     * @param n the Binet index
+     * @return the results.
      */
-    public static long FibBinet(int n) {
+    public static long fibBinet(int n) {
         double phi = (1 + Math.sqrt(5)) / 2.0;
-        double psi = (1 + Math.sqrt(5)) / 2.0;
+        double psi = (1 - Math.sqrt(5)) / 2.0;
         double results = (Math.pow(phi, n) - Math.pow(psi, n)) / Math.sqrt(5);
 
         return Math.round(results);
@@ -52,7 +58,7 @@ public class ERAFibonacci {
         System.out.println("N\tTime");
         for (int n = 1000; n <= 100000; n += 1000) {
             long start = System.nanoTime();
-            Fibonacci(n);
+            fibonacci(n);
             long end = System.nanoTime();
             double time = (end - start) / 1e9;
             System.out.println(n + "\t" + time);
